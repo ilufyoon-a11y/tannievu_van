37b1c1679552094cf81f341d6fd32bac6f5d67e0
ilufyoon-a11y <ilufyoon@gmail.com>
2026-07-06 17:56:29 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -51,6 +51,7 @@ GIF_RECHAZADO  = "https://i.postimg.cc/MTXZnXd8/1000005045.jpg"
 GIF_COMANDOS   = "https://i.postimg.cc/6qjQHnqv/1000005043-(1).jpg"
 GIF_CHARADA    = "https://i.postimg.cc/ryb94Wgj/1000004755.jpg"
 GIF_CASERIA    = "https://i.postimg.cc/K8Y95C9Y/upscalemedia-transformed-(9).jpg"
+GIF_SONG = "https://i.postimg.cc/RV1Fp3bb/upscalemedia-transformed-(11).jpg"
 GIF_PIRATA     = "https://i.postimg.cc/ryb94Wgj/1000004755.jpg"
 GIF_MAYOROMENOR = "https://i.postimg.cc/ryb94Wgj/1000004755.jpg"
 
